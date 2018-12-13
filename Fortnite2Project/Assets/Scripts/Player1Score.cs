d1d94a3d574c8771a031fe68aa46b84e8640a0e6
edelamaza <edelamaza@me.com>
2018-12-13 13:56:55 -0800

    Added Win&Loss Condition

--- Fortnite2Project/Assets/Scripts/Player1Score.cs
+++ Fortnite2Project/Assets/Scripts/Player1Score.cs
@@ -6,14 +6,26 @@ using UnityEngine.UI;
 public class Player1Score : MonoBehaviour {
 
     public Text score1;
+    public Text win;
+
+    public Rigidbody rb1;
+    public Rigidbody rb2;
 
     public int currentScore1;
+    public int maxScore;
 
 
     public void Score()
     {
         currentScore1 += 1;
         score1.text = currentScore1 + "";
+
+        if (currentScore1 == maxScore)
+        {
+            win.text = "Player 1 WINS!";
+            rb1.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+            rb2.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+        }
     }
 
 
--- Fortnite2Project/Assets/Scripts/Player2Score.cs
+++ Fortnite2Project/Assets/Scripts/Player2Score.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class Player2Score : MonoBehaviour
 {
-
+    public int maxScore;
+    public Rigidbody rb1;
+    public Rigidbody rb2;
     public Text score1;
+    public Text win;
 
     public int currentScore1;
 
@@ -16,6 +19,13 @@ public class Player2Score : MonoBehaviour
 
        currentScore1 += 1;
         score1.text = currentScore1.ToString();
+
+        if (currentScore1 == maxScore)
+        {
+            win.text = "Player 2 WINS!";
+            rb1.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+            rb2.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+        }
     }
 
 
